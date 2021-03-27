3e87fa3ec7f5a501f5338bab1f14b94efbad219c
Yosuke Miyajima <mayo.suke@gmail.com>
2021-03-27 21:56:28 +0900

    Start ABC087B

---
+++ test_ABC083B.py
@@ -0,0 +1,15 @@
+# https://atcoder.jp/contests/abs/tasks/abc083_b
+
+
+def sol(N, A, B):
+    return 0
+
+def test_1():
+    assert sol(20, 2, 5) == 84
+
+def test_2():
+    assert sol(10, 1, 2) == 13
+
+def test_3():
+    assert sol(100, 4, 16) == 4554
+
