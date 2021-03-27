83f56b1e7d18dbbb9592802c6c84dbfbb9a65aa0
Yosuke Miyajima <mayo.suke@gmail.com>
2021-03-27 22:21:51 +0900

    Add solution for ABC083B

--- test_ABC083B.py
+++ test_ABC083B.py
@@ -1,8 +1,15 @@
 # https://atcoder.jp/contests/abs/tasks/abc083_b
 
+def sum_of_each_digit(N):
+    return sum([int(d) for d in str(N)])
 
 def sol(N, A, B):
-    return 0
+    ans = 0
+    for n in range(1, N+1):
+        soed = sum_of_each_digit(n)
+        if soed >= A and soed <= B:
+            ans += n
+    return ans
 
 def test_1():
     assert sol(20, 2, 5) == 84
@@ -13,3 +20,19 @@ def test_2():
 def test_3():
     assert sol(100, 4, 16) == 4554
 
+
+# passed solution:
+# N, A, B = [int(s) for s in input().split()]
+
+# def sum_of_each_digit(N):
+#   return sum([int(d) for d in str(N)])
+
+# def sol(N, A, B):
+#   ans = 0
+#   for n in range(1, N+1):
+#     soed = sum_of_each_digit(n)
+#     if soed >= A and soed <= B:
+#       ans += n
+#   return ans
+
+# print(sol(N, A, B))
