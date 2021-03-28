9083e2ed9aed712e9fea0ee9f472b4d361c89c1d
Yosuke Miyajima <mayo.suke@gmail.com>
2021-03-28 12:15:59 +0900

    Start ABC088B

---
+++ test_ABC088B.py
@@ -0,0 +1,18 @@
+# https://atcoder.jp/contests/abs/tasks/abc088_b
+
+
+def sol(N, A):
+    ans = 0
+    return ans
+
+def test_1():
+    assert sol(2, [3, 1]) == 2
+
+def test_2():
+    assert sol(3, [2, 7, 4]) == 5
+
+def test_3():
+    assert sol(4, [20, 18, 2, 18]) == 18
+
+
+# passed solution:
