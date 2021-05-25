dd23729d3edcc3b1a10610ca023c8b7865731010
Yosuke Miyajima <mayo.suke@gmail.com>
2021-05-26 00:38:35 +0900

    Add another solution for ABC044B

--- test_ABC044B.py
+++ test_ABC044B.py
@@ -11,8 +11,15 @@ def sol1(w):
             return 'No'
     return 'Yes'
 
+# AC (refered: https://atcoder.jp/contests/abc044/submissions/22731036)
+def sol2(w):
+    for c in w:
+        if w.count(c) % 2:
+            return 'No'
+    return 'Yes'
+
 def sol(w):
-    return sol1(w)
+    return sol2(w)
 
 def test_1():
     assert sol('abaccaba') == 'Yes'
@@ -26,6 +33,15 @@ def test_2():
 # count = Counter(w)
 # for c in count.values():
 # 	if c % 2:
+# 		print('No')
+# 		break
+# else:
+# 	print('Yes')
+
+# sol2
+# w = input()
+# for c in w:
+# 	if w.count(c) % 2:
 # 		print('No')
 # 		break
 # else:
